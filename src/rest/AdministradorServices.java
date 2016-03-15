391a2dcab20ddbb794e8fb1324335a0beb25cfb2
d.marino10 <d.marino10@ISC218.sis.virtual.uniandes.edu.co>
2016-03-15 12:55:08 -0500

    wartfawe

--- src/rest/AdministradorServices.java
+++ src/rest/AdministradorServices.java
@@ -3,7 +3,9 @@ package rest;
 import java.util.List;
 
 import javax.servlet.ServletContext;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
@@ -56,5 +58,26 @@ public class AdministradorServices {
 				return Response.status(500).entity(doErrorMessage(e)).build();
 			}
 			return Response.status(200).entity(videos).build();
+		}
+		
+		
+		/**
+	     * Método que expone servicio REST usando PUT que agrega el video que recibe en Json
+	     * <b>URL: </b> http://"ip o nombre de host":8080/VideoAndes/rest/videos/video
+	     * @param admi - video a agregar
+	     * @return Json con el video que agrego o Json con el error que se produjo
+	     */
+		@PUT
+		@Path("/administrador")
+		@Consumes(MediaType.APPLICATION_JSON)
+		@Produces(MediaType.APPLICATION_JSON)
+		public Response addAdministrador(Administrador admi) {
+			PuertoAndesMaster tm = new PuertoAndesMaster(getPath());
+			try {
+				tm.addAdministrador(admi);
+			} catch (Exception e) {
+				return Response.status(500).entity(doErrorMessage(e)).build();
+			}
+			return Response.status(200).entity(admi).build();
 		}
 }
--- src/tm/PuertoAndesMaster.java
+++ src/tm/PuertoAndesMaster.java
@@ -343,5 +343,41 @@ public class PuertoAndesMaster {
 		
 	}
 
+	/**
+	 * Método que modela la transacción que agrega un solo video a la base de datos.
+	 * <b> post: </b> se ha agregado el video que entra como parámetro
+	 * @param admi - el video a agregar. video != null
+	 * @throws Exception - cualquier error que se genera agregando el video
+	 */
+	public void addAdministrador(Administrador admi) throws Exception {
+		DAOTablaAdministradores dao = new DAOTablaAdministradores();
+		try 
+		{
+			//////Transacción
+			this.conn = darConexion();
+			dao.setConn(conn);
+			dao.addAdministrador(admi);
+			conn.commit();
+
+		} catch (SQLException e) {
+			System.err.println("SQLException:" + e.getMessage());
+			e.printStackTrace();
+			throw e;
+		} catch (Exception e) {
+			System.err.println("GeneralException:" + e.getMessage());
+			e.printStackTrace();
+			throw e;
+		} finally {
+			try {
+				dao.cerrarRecursos();
+				if(this.conn!=null)
+					this.conn.close();
+			} catch (SQLException exception) {
+				System.err.println("SQLException closing resources:" + exception.getMessage());
+				exception.printStackTrace();
+				throw exception;
+			}
+		}
+	}
 	
 }
